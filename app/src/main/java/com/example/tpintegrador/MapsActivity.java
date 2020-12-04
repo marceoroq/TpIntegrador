56e99894b17b76aefc896ac3ca3a5df2467769a1
mart-dominguez <mdomingu@gmail.com>
2020-12-03 21:38:30 -0300

    Nuevo cambio

--- app/src/main/java/com/example/tpintegrador/MapsActivity.java
+++ app/src/main/java/com/example/tpintegrador/MapsActivity.java
@@ -89,6 +89,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //
     }
 
     @Override
